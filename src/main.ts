56aa329cf04923c2d063b41b2e0df778e17b176f
acevedonm <acevedonm@gmail.com>
2021-12-28 20:31:31 -0300

    change port

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { initSwagger } from './app.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const logger = new Logger();
   initSwagger(app);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  const port = 3000;
+  const port = 3001;
   await app.listen(port);
   logger.log(`Lares listen in port: ${port}`);
 }
